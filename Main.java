919f8fe42bbeed32d48341aeb8e0a53125faf962
Ruth <88747761+AwareRuuh@users.noreply.github.com>
2022-11-05 17:10:04 +0000

    • Entrada: 86399
    • Saída: 23:59:59

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+  int segundos = 86399;
+  int horas = segundos / 3600;
+    segundos %= 3600;
+  int minutos = segundos / 60;
+    segundos %= 60;
+System.out.print("Tempo total: ");
+if (horas >= 0)
+    System.out.printf("%d:", horas);
+if (minutos >= 0)
+    System.out.printf("%d:", minutos);
+if (segundos >= 0)
+    System.out.printf("%d:", segundos);
   }
 }
